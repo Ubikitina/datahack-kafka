2fa91dce804ef39fd1295006bb60052d616354c8
Maialen <34455658+Ubikitina@users.noreply.github.com>
2024-06-06 19:35:44 +0200

    Add analyzer.

--- analyzer/app.py
+++ analyzer/app.py
@@ -1,31 +1,93 @@
-from transformers import pipeline
-from kafka import KafkaConsumer
+#!/usr/bin/env python
 
+import json
+import avro.schema
+import avro.io
+from kafka import KafkaConsumer, KafkaProducer
+from io import BytesIO
+from transformers import pipeline
+import logging
+
+
+# Kafka consumer configuration
+kafka_brokers = ['broker']
+input_topic_name = 'tweets-input'
+output_topic_name = 'analyzed-tweets'
+
+# Definir el esquema de avro esperado
+avro_schema = {
+  "type": "record",
+  "name": "schema",
+  "namespace": "tweets.input.value",
+  "connect.name": "tweets.input.value.schema",
+  "fields": [
+    {"name": "tweet_id","type": "long"},
+    {"default": None,"name": "date","type": ["null","string"]},
+    {"default": None,"name": "username","type": ["null","string"]},
+    {"default": None,"name": "tweet","type": ["null","string"]}
+  ] 
+}
+
+# Parsear esquema
+schema = avro.schema.parse(json.dumps(avro_schema))
+
+# Iniciar el sentiment pipeline
 sentiment_pipeline = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
 
-# Solicitar tres cadenas de texto al usuario
-# tweet1 = input("Ingresa el primer tweet: ")
-# tweet2 = input("Ingresa el segundo tweet: ")
-# tweet3 = input("Ingresa el tercer tweet: ")
-
-# array_of_tweets = [tweet1, tweet2, tweet3]
-
-# print("Estoy analizando tus Tweets, en seguida te digo su mood ;)")
-# print(sentiment_pipeline(array_of_tweets))
+log = logging.getLogger(__name__)
 
 
+# Función para decodificar el mensaje Avro
+def decode_avro_message(message_value, schema):
+    bytes_reader = BytesIO(message_value)
+    decoder = avro.io.BinaryDecoder(bytes_reader)
+    reader = avro.io.DatumReader(schema)
+    return reader.read(decoder)
 
-consumer = KafkaConsumer('tweets-input',
-                         group_id='input-group',
-                         bootstrap_servers=['127.0.0.1:9092'],
+# Funcion para el caso de exito en la produccion del metodo
+def on_send_success(record_metadata):
+  print(record_metadata.topic)
+  print(record_metadata.partition)
+  print(record_metadata.offset)
+
+# Funcion para el caso de error
+def on_send_error(ex):
+  log.error('I am an Error', exc_info=ex)
+  # handle exception
+
+
+# Crear el consumer
+consumer = KafkaConsumer(input_topic_name,
+                         group_id='py-group',
+                         bootstrap_servers=kafka_brokers,
                          auto_offset_reset='earliest')
 
-consumer.subscribe(['tweets-input'])
+consumer.subscribe([input_topic_name])
 
+# Crear el producer
+producer = KafkaProducer(
+    bootstrap_servers=kafka_brokers,
+    value_serializer=lambda m: json.dumps(m).encode('utf-8')) # Utilizaremos la libreria JSON para serializar nuestros mensajes
+
+
+# Para cada mensaje consumido por el consumer
 for message in consumer:
-  # message value and key are raw bytes -- decode if necessary!
-  # e.g., for unicode: `message.value.decode('utf-8')`
-  print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                       message.offset,
-                                       message.key.decode('utf-8'),
-                                       message.value.decode('latin-1')))
+  # Decodificamos el mensaje y construimos el diccionario. Con [5:] eliminamos el encabezado mágico y el ID de esquema (primeros 5 bytes)
+  message_dict = decode_avro_message(message.value[5:], schema)
+  # Example: {'tweet_id': 2018926307, 'date': 'Wed Jun 03 10:43:39 PDT 2009', 'username': 'Mia_R', 'tweet': "@simalves LOL i was contemplating on doing the same thing Sim! but i really need to sleep... i'll stay for a few more minutes... "}
+  
+  # Analizamos el sentimiento del tweet
+  sentiment = sentiment_pipeline([message_dict.get('tweet')])[0] # Example: {'label': 'POS', 'score': 0.5627670288085938}
+  
+  # Combinamos los diccionarios
+  message_dict.update({'sentiment': sentiment})
+  # Example: {'tweet_id': 2018926307, 'date': 'Wed Jun 03 10:43:39 PDT 2009', 'username': 'Mia_R', 'tweet': "@simalves LOL i was contemplating on doing the same thing Sim! but i really need to sleep... i'll stay for a few more minutes... ", 'sentiment': {'label': 'POS', 'score': 0.5627670288085938}}
+
+  # Imprimimos el mensaje de evento analizado
+  print(f"Event analyzed: {message_dict}")
+
+  # A traves del producer, enviamos el evento al topic de salida
+  producer.send(output_topic_name, key=str(message_dict.get('tweet_id')).encode('utf-8'),
+                value=message_dict).add_callback(on_send_success).add_errback(on_send_error)
+  producer.flush()
+  print("Event sent.")
--- doc/Development notes.md
+++ doc/Development notes.md
@@ -8,7 +8,7 @@ https://www.kaggle.com/datasets/kazanova/sentiment140
 
 Lo he ubicado en la carpeta [dataset](../dataset/).
 
-## Conectar archivo con un topic de Kafka
+## Conectar archivo con un topic de Kafka utilizando un source connector
 
 https://docs.confluent.io/kafka-connectors/spooldir/current/connectors/csv_source_connector.html#spooldir-csv-source-connector
 
@@ -27,7 +27,45 @@ Entramos al contenedor del broker:
 docker exec -it broker /bin/bash
 ```
 
+O a la web de Control Center:
+http://localhost:9021/clusters
+
 Consumimos los mensajes del topic `tweets-input`:
 ```bash
 kafka-console-consumer --bootstrap-server broker:9092 --topic tweets-input --from-beginning
 ```
+
+## Crear el contenedor con el analizador de sentimientos
+
+Especificaciones:
+- El contenedor debe consumir del topic de kafka `tweets-input`
+- Debe analizar los sentimientos de los tweets uno a uno - real time.
+- Envía el resultado al topic de kafka `analyzed-tweets`
+
+### Pruebas desde consola
+
+Para correr el archivo desde consola, con el entorno de `predictor-env`:
+```bash
+conda env --list
+conda activate predictor-env
+python ./analyzer/app.py
+```
+
+Para copiar más archivos dentro del contenedor y comprobar que sigue consumiendo:
+```bash
+docker cp ./connectors/plaintext/dataset/twitter-extract-02.csv connect:/tmp/input/twitter-extract-02.csv
+docker cp ./connectors/plaintext/dataset/twitter-extract-03.csv connect:/tmp/input/twitter-extract-03.csv
+```
+
+### Pruebas con el contenedor
+
+Primero tengo que testear que el contenedor funciona:
+
+```bash
+cd analyzer
+docker build -t sentiment_analyzer:v1 .
+
+docker run -it sentiment_analyzer:v1
+```
+
+Después integraremos en el docker-compose.yml.
